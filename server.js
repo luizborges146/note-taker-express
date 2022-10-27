6f66d0f29af0dfac1397bce62373a63a735da878
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-27 16:07:57 -0700

    Create the first app.get that will link it to note.html

--- server.js
+++ server.js
@@ -5,14 +5,31 @@ const {v4:uuidv4} = require('uuid');
 //to access the DB folder, file db.json
 const dbJson = require("./db/db.json");
 
-
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
+app.get('/notes', (req,res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
+
+app.get('/api/notes', (req,res) => {
+    const dNotes = fs.readFile(path.join(__dirname,"./db/db.jsib"), "utf-8");
+    const parseNotes = JSON.parse(dNotes);
+    res.json(parseNotes);
+})
+
+app.post('/api/notes', (req,res) => {
+    const dNotes = fs.readFile(path.join(__dirname,"./db/db.jsib"), "utf-8");
+    const parseNotes = JSON.parse(dNotes);
+    req.body.id = uuidv4();
+    parseNotes.push(req.body);
+}
 
 
 
+// req.body.id = uuidv4();
+// parseNotes
