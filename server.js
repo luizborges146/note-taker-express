7eb816cfef62222add1d97e7c52ba968f3fb488b
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-27 22:26:13 -0700

    had an issue with a typo, where I miss spell json on the file and also added a better name for the const dbJson

--- server.js
+++ server.js
@@ -4,27 +4,28 @@ const path = require("path");
 const {v4:uuidv4} = require('uuid');
 
 //to access the DB folder, file db.json
-const dbJson = require("./db/db.json");
+const databaseJson = require("./db/db.json");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static("public"));
+
 
 app.get('/notes', (req,res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
 
 app.get('/api/notes', (req,res) => {
-    const dNotes = fs.readFile(path.join(__dirname,"./db/db.jsib"), "utf-8");
+    const dNotes = fs.readFileSync(path.join(__dirname,"./db/db.jsib"), "utf-8");
     const parseNotes = JSON.parse(dNotes);
     res.json(parseNotes);
 });
 
 app.post('/api/notes', (req,res) => {
-    const dNotes = fs.readFile(path.join(__dirname,"./db/db.jsib"), "utf-8");
+    const dNotes = fs.readFileSync(path.join(__dirname,"./db/db.json"), "utf-8");
     const parseNotes = JSON.parse(dNotes);
     req.body.id = uuidv4();
     parseNotes.push(req.body);
@@ -35,26 +36,24 @@ app.post('/api/notes', (req,res) => {
 
 app.get('*', (req,res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
-app.delete("/apinotes/:id",(req,res) => {
+app.delete("/api/notes/:id",(req,res) => {
     
     let delNotes = parseInt(req.params.id);
 
-    for(let i = 0; i < dbJson.length; i++) {
-        if (delNotes === dbJson[i].id) {
-            dbJson.splice(i,1)
+    for(let i = 0; i < databaseJson.length; i++) {
+        if (delNotes === databaseJson[i].id) {
+            databaseJson.splice(i,1)
 
-            let noteJson = JSON.stringify(dbJson,null,2);
+            let noteJson = JSON.stringify(databaseJson,null,2);
 
-            fs.writeFile("./db/db.json", noteJson, (err) => {
+            fs.writeFile("./db/db.json", noteJson, function(err) {
                 if(err) throw err;
                 console.log("The note selected has been deleted!");
-                res.json(dbJson)
+                res.json(databaseJson)
             })
         }
     }
 })
-
-
 
 
 app.listen(PORT,() => console.log(`App listening at http://localhost:${PORT} 🚀`));
